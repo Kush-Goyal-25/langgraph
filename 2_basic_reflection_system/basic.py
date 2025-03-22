7055d0f66887a905ad681420be89ef511c60cafc
Kush-Goyal-25 <kush.goyal.personal@gmail.com>
2025-03-22 22:04:13 +0530

    commiting basics in 2nd folder

--- 2_basic_reflection_system/basic.py
+++ 2_basic_reflection_system/basic.py
@@ -1,28 +1,75 @@
+# from typing import List, Sequence
+#
+# from chains import generation_chain, reflection_chain
+# from dotenv import load_dotenv
+# from langchain.graph import END, MessageGraph
+# from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+#
+# load_dotenv()
+#
+# graph = MessageGraph()
+#
+# REFLECT = "reflect"
+# GENERATE = "generate"
+#
+#
+# def generate_node(state):
+#     return generation_chain.invoke({"messages": state})
+#
+#
+# def reflect_node(state):
+#     response = reflection_chain.invoke({"messages": state})
+#     return [HumanMessage(content=response.content)]
+#
+#
+# graph.add_node(GENERATE, generate_node)
+# graph.add_node(REFLECT, reflect_node)
+#
+# graph.set_entry_point(GENERATE)
+
+
 from typing import List, Sequence
 
 from chains import generation_chain, reflection_chain
 from dotenv import load_dotenv
-from langchain.graph import END, MessageGraph
-from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
 
 load_dotenv()
 
-graph = MessageGraph()
-
 REFLECT = "reflect"
 GENERATE = "generate"
+graph = MessageGraph()
 
 
 def generate_node(state):
     return generation_chain.invoke({"messages": state})
 
 
-def reflect_node(state):
-    response = reflection_chain.invoke({"messages": state})
+def reflect_node(messages):
+    response = reflection_chain.invoke({"messages": messages})
     return [HumanMessage(content=response.content)]
 
 
 graph.add_node(GENERATE, generate_node)
 graph.add_node(REFLECT, reflect_node)
-
 graph.set_entry_point(GENERATE)
+
+
+def should_continue(state):
+    if len(state) > 6:
+        return END
+    return REFLECT
+
+
+graph.add_conditional_edges(GENERATE, should_continue)
+graph.add_edge(REFLECT, GENERATE)
+
+app = graph.compile()
+
+print(app.get_graph().draw_mermaid())
+app.get_graph().print_ascii()
+
+response = app.invoke(HumanMessage(content="AI Agents taking over content creation"))
+
+print(response)
